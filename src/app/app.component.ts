fffcd964834e4201031761168f0e4db0e1b119a7
TMA\intern.tdthienluan <trandaothienluan@gmail.com>
2022-04-20 17:42:20 +0700

    Update list item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
         ['Medical instruments','Kittest']
         ),
       new Product(
-        'Medical mask type 1',
+        'Mask Famapro VN95',
         '/assets/images/mask.jpg',
         1,
         ['Mask','Medical mask type 1']
@@ -29,8 +29,110 @@ export class AppComponent {
         'Siro Prospan',
         '/assets/images/siro.jpg',
         10,
-        ['Medicine','Siro']
-        )
+        ['Medicine','Cough medicine']
+        ),
+      new Product(
+        'Aspirin',
+        '/assets/images/aspirin.jpg',
+        4,
+        ['Medicine','Headache medicine']
+        ),
+      new Product(
+        'Eugica',
+        '/assets/images/eugica.jpg',
+        7,
+        ['Medicine','Cough medicine']
+        ),
+      new Product(
+        'Eve Quick',
+        '/assets/images/eve.jpg',
+        4,
+        ['Medicine','Headache medicine']
+        ),
+      new Product(
+        'Panadol Extra',
+        '/assets/images/panadol.jpg',
+        6,
+        ['Medicine','Headache medicine']
+        ),
+      new Product(
+        'Paracetamol',
+        '/assets/images/paracetamol.jpg',
+        8,
+        ['Medicine','Headache medicine']
+        ),
+      new Product(
+        'Berberin',
+        '/assets/images/berberin.jpg',
+        6,
+        ['Medicine','Stomach medicine']
+        ),
+      new Product(
+        'Berocca',
+        '/assets/images/berocca.jpg',
+        7,
+        ['Food Supplement','effervescent medicine']
+        ),
+      new Product(
+        'Loperamid',
+        '/assets/images/loperamid.jpg',
+        5,
+        ['Medicine','Stomach medicine']
+        ),
+      new Product(
+        'My VIta',
+        '/assets/images/myvita.jpg',
+        3,
+        ['Food Supplement','effervescent medicine']
+        ),
+      new Product(
+        'Strepsils',
+        '/assets/images/strepsils.jpg',
+        2,
+        ['Food Supplement','lozenges']
+        ),
+      new Product(
+        'Vitamin C lozenges',
+        '/assets/images/vitaminC.jpg',
+        2,
+        ['Food Supplement','lozenges']
+        ),
+      new Product(
+        'GMask',
+        '/assets/images/gmask.jpg',
+        1,
+        ['Mask','Medical mask type 2']
+        ),
+      new Product(
+        'Busrel Mask',
+        '/assets/images/busrelmask.jpg',
+        1,
+        ['Mask','Medical mask type 1']
+        ),
+      new Product(
+        'Urevo Mask',
+        '/assets/images/urevomask.jpg',
+        1,
+        ['Mask','Medical mask type 2']
+        ),
+      new Product(
+        'Wheel chair black',
+        '/assets/images/wheelchair.jpg',
+        100,
+        ['Medical Instruments','Wheel chair type 1']
+        ),
+      new Product(
+        'Needle',
+        '/assets/images/needle.jpg',
+        3,
+        ['Medical Instruments','Neelde type 1']
+        ),
+       new Product(
+        'Wood Crutch',
+        '/assets/images/crutch.jpg',
+        3,
+        ['Medical Instruments','Crutch']
+        ),
     ];
   }
 
