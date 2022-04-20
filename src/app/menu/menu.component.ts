64e85c6fbdc715af1ef0819c8473ee2891f90851
TMA\intern.tdthienluan <trandaothienluan@gmail.com>
2022-04-20 16:10:57 +0700

    Update code 20/4/2022

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -16,39 +16,38 @@ export class MenuComponent implements OnInit {
 
   // Your initial click listener on the host element
  @HostListener('click', ['$event'])onClick(event:any) {
-  event.stopPropagation();
+  //  event.stopPropagation();
 if (event.target.id == "collapseBtn") {
   document.getElementsByClassName('sidebar')[0].classList.add('showsidebar');
   document.body.classList.add('push');
   this.sideBarOpen = true;
-} else {
-if (this.sideBarOpen) {
-   document.getElementsByClassName('sidebar')[0].classList.remove('showsidebar');
-   document.body.classList.remove('push');
-   this.sideBarOpen = false;
 }
-}
+//  else {
+// if (this.sideBarOpen) {
+//    document.getElementsByClassName('sidebar')[0].classList.remove('showsidebar');
+//    document.body.classList.remove('push');
+//    this.sideBarOpen = false;
+// }
+// }
 }
 
 // Click listener on the window object to handle clicks anywhere on 
 // the screen.
 @HostListener('window:click', ['$event']) onOutsideClick(event:any){
 if(this.sideBarOpen && !this.el.nativeElement.contains(event.target)){
-  this.sideBarOpen=false;
   document.getElementsByClassName('sidebar')[0].classList.remove('showsidebar');
   document.body.classList.remove('push');
+  this.sideBarOpen=true;
 }
 }
 
   ngOnInit() {
   }
 
-  toggleSideBar() {
-    document.getElementsByClassName('sidebar')[0].classList.add('showsidebar');
-    this.sideBarOpen = true;
- }
+//   toggleSideBar() {
+//     document.getElementsByClassName('sidebar')[0].classList.add('showsidebar');
+//     this.sideBarOpen = true;
+//  }
 
-  // toggleSidebar() {
-  //   this.collapse = !this.collapse;
-  // }
+ 
 }
