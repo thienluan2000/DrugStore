c7766d24b6aaf9cf30150b0935b47f48bace3688
thienluan2000 <trandaothienluan@gmail.com>
2022-05-14 01:02:35 +0700

    Update searched list page (reload page after search)

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -4,25 +4,30 @@ import { ProductService } from 'src/services/product.service';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-search-bar',
-    templateUrl: './search-bar.component.html',
-    styleUrls: ['./search-bar.component.scss']
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-    @Input() productList!: IProductModel[];
-    public searchKeywords!: string;
+  @Input() productList!: IProductModel[];
+  public searchKeywords!: string;
 
-    constructor(
-        private productService: ProductService,
-        private router: Router) {
-    }
-    ngOnInit(): void {
+  constructor(
+    private productService: ProductService,
+    private router: Router) {
+  }
+  ngOnInit(): void {
 
-    }
+  }
 
-    public doSearch() {
-        // this.productService.getGlobalSearchList(this.searchKeywords);
-        this.router.navigate(['/'], { queryParams: { key: this.searchKeywords } });
-    }
+  public doSearch() {
+    // this.productService.getGlobalSearchList(this.searchKeywords);
+    this.router.navigate(['/'], { queryParams: { key: this.searchKeywords } });
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
+
 
 }
