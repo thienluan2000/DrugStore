12cb0cfe91e8cfc46f8e61a006e08efd221f8fdf
thienluan2000 <trandaothienluan@gmail.com>
2022-05-17 04:31:49 +0700

    no message

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'price-display',
-  template:  `<div class="price-display" style="font-weight: bold; color:green"> \{{ price }} $</div> `},
+  template:  `<div class="price-display" style="font-weight: bold; color:rgb(7, 158, 45)"> \{{ price }} $</div> `},
 )
 export class PriceDisplayComponent  {
   @Input() price!: number;
