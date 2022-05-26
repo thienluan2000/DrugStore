cbf0414eb9440f8c4cf65bd50774279edb52bc88
thienluan2000 <trandaothienluan@gmail.com>
2022-05-26 15:28:39 +0700

    Work with responsive

--- src/app/product-col/product-col.component.ts
+++ src/app/product-col/product-col.component.ts
@@ -3,19 +3,18 @@ import { IProductModel } from '../../models/product.model';
 import { CartService } from 'src/services/cart.service';
 
 @Component({
-    selector: 'product-col',
-    templateUrl: './product-col.component.html',
-    styleUrls: ['./product-col.component.scss']
+  selector: 'product-col',
+  templateUrl: './product-col.component.html',
+  styleUrls: ['./product-col.component.scss']
 })
 export class ProductColComponent {
-    @Input() product!: IProductModel | null;
-    // public selectedProduct: IProductModel | null = null;
-    constructor(private cartService: CartService) { }
-    addToCart(product: IProductModel) {
-        this.cartService.addToCart(product);
-        window.alert('Your product has been added to the cart!');
-
-    }
+  @Input() product!: IProductModel | null;
+  // public selectedProduct: IProductModel | null = null;
+  constructor(private cartService: CartService) { }
+  addToCart(product: IProductModel) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
 
 }
